7ce436140f3b3e53f8847c21ba14cdf0e9f04314
OlivierGdM <OlivierGdM@users.noreply.github.com>
2026-04-17 16:37:29 +0200

    Added log copy in terminal for debugging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from serial_reader import read_serial
-from logger import write_log
+from app.serial_reader import read_serial
+from app.logger import write_log
 
 def main() :
     for line in read_serial() : 
         write_log(line)
+        print(line)
