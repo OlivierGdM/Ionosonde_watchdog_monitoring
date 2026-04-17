261b50b0c06be7325ead87ee60496daeccca02e0
OlivierGdM <OlivierGdM@users.noreply.github.com>
2026-04-17 11:05:57 +0200

    Created reader and writer functions

--- app/serial_reader.py
+++ app/serial_reader.py
@@ -1,1 +1,11 @@
 import serial
+
+def read_serial(port="/dev/ttyUSB0", baudrate=9600) :
+    ser = serial.Serial(port, baudrate, timeout=1)
+
+    while True :
+        # Read the oldest line in the USB driver buffer
+        line = ser.readline().decode(errors="ignore").strip()
+
+        if line :
+            yield line
